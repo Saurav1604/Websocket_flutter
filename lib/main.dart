02c47912e8059a367a3c3b580ddbad212a06c7d2
Saurav Vinodh Kumar <SauravVinodh_Kumar@comcast.com>
2026-02-17 12:22:05 +0530

    New changes 17/2/26-1222

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
-import 'dart:async';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:web_socket_channel/html.dart' if (dart.library.io) 'package:web_socket_channel/io.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,94 +13,135 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const WebSocketScreen(),
+      title: 'Chatting App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
-class WebSocketScreen extends StatefulWidget {
-  const WebSocketScreen({super.key});
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  // ignore: library_private_types_in_public_api
-  _WebSocketScreenState createState() => _WebSocketScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
-class _WebSocketScreenState extends State<WebSocketScreen> {
-  final TextEditingController _controller = TextEditingController();
-  late WebSocketChannel _channel;
-  late StreamSubscription _subscription;
-  String receivedMessage = "";
-  String errorMessage = "";
-  @override
-  void initState() {
-    super.initState();
-    try {
-      _channel = WebSocketChannel.connect(
-        Uri.parse('wss://echo.websocket.org'),
-      );
-      _subscription = _channel.stream.listen(
-        (message) {
-          setState(() {
-            receivedMessage = message;
-            errorMessage = "";
-          });
-        },
-        onError: (error) {
-          setState(() {
-            errorMessage = 'Error: $error';
-          });
-        },
-        onDone: () {
-          setState(() {
-            errorMessage = 'Connection closed';
-          });
-        },
-      );
-    } catch (e) {
-      setState(() {
-        errorMessage = 'Failed to connect: $e';
-      });
-    }
-  }
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final TextEditingController one = TextEditingController();
+
+  // User-defined TextField dimensions
+  static const double textFieldWidth = 300.0;  // Customize width here
+  static const double textFieldHeight = 60.0;  // Customize height here
+  static const int maxLines = 1;  // Number of lines
+  static const int maxCharacters = 100;  // Maximum character length
+  late String receivedMessage = "";
+  late String message = "no message yet";
   void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      _channel.sink.add(_controller.text);
-    }
-  }
-  @override
-  void dispose() {
-    _subscription.cancel();
-    _channel.sink.close();
-    _controller.dispose();
-    super.dispose();
+    if (one.text.isEmpty) return;  // Guard clause
+    setState(() {
+      message = one.text;
+      receivedMessage += (receivedMessage.isEmpty ? '' : '\n') + message;
+    });
+    log('Message sent: $message');
+    one.clear();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter WebSocket Demo')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(labelText: 'Enter message'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _sendMessage,
-              child: const Text('Send Message'),
-            ),
-            const SizedBox(height: 20),
-            if (errorMessage.isNotEmpty)
-              Text(
-                errorMessage,
-                style: const TextStyle(color: Colors.red),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            
+          ),
+          child: Text(widget.title),
+        ), 
+      ),
+
+
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Left half - Text
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Enter message:',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration:BoxDecoration(
+                        border :Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(4)
+                      ),
+                      child: Text(
+                        '-----Message History-----\n\n${receivedMessage.isEmpty? 'No messages' : receivedMessage}',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            if (receivedMessage.isNotEmpty)
-              Text('Received: $receivedMessage'),
+            ),
+            // Right half - TextField
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: textFieldWidth,  // User-defined width
+                      height: textFieldHeight,  // User-defined height
+                      child: TextField(
+                        controller: one,
+                        maxLength: maxCharacters,
+                        maxLines: maxLines,  // Control vertical size
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Type here',
+                          counterText: '',  // Hide character counter to save space
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _sendMessage,
+                      child: const Text('Send'),
+                      ),
+                      Text("Received message : $message"),
+                  ],
+                ),
+                
+              ),
+            ),
           ],
         ),
       ),
+      //floatingActionButton: FloatingActionButton(
+        //onPressed: _incrementCounter,
+        //tooltip: 'Increment',
+        //child: const Icon(Icons.add),
+      //),
     );
+  }
+  @override
+  void dispose() {
+    one.dispose();
+    super.dispose();
   }
 }
